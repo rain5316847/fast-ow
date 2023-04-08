78976fc6edf4b2df6c04df5649fe30d4bda047df
Crank <1300833701@qq.com>
2023-04-08 08:15:07 +0800

    first commit

---
+++ git-test/fast-ow/fast-local/src/main/java/com/fast/fastlocal/FastLocalApplication.java
@@ -0,0 +1,13 @@
+package com.fast.fastlocal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastLocalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FastLocalApplication.class, args);
+    }
+
+}
---
+++ git-test/fast-ow/fast-local/src/test/java/com/fast/fastlocal/FastLocalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fast.fastlocal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FastLocalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
